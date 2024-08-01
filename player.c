d63d9109119797534d5341608884b02ff287c4ef
Hugo Marta <hugollmarta@hotmail.com>
2024-08-01 23:47:35 +0100

    upt player init

--- player.c
+++ player.c
@@ -37,6 +37,15 @@ void init_player(t_game *game)
     game->p1.right_sprites[0] = ft_strdup(PLAYER_RIGHT_MV1);
     game->p1.right_sprites[1] = ft_strdup(PLAYER_RIGHT_MV2);
     game->p1.right_sprites[2] = ft_strdup(PLAYER_RIGHT_MV3);  
+    
+    game->p1.dx = 0;
+    game->p1.dy = 0;
+    game->p1.steps_remaining = 0;
+    game->p1.step_size = 0;
+    game->p1.update_interval = 1000 / 60;
+    game->p1.last_update_time = clock();
+    game->p1.accumulated_time = 0;
+
 }
 /*
 void    create_player(t_sprite *sprite, t_game *game, int posx, int posy)
@@ -106,3 +115,34 @@ void	put_player(t_game *game, int sprite_index)
     game->p1.p1 = create_sprite(game, current_sprite);
     create_character(game->p1.p1, game, x, y);
 }
+/*
+void put_player(t_game *game, int sprite_index)
+{
+    char *current_sprite;
+    int x = game->p1.p1_p.x;
+    int y = game->p1.p1_p.y;
+
+    if (game->p1.mv_dir == DIR_UP)
+    {
+        current_sprite = game->p1.back_sprites[sprite_index];
+    }    
+    else if (game->p1.mv_dir == DIR_DOWN)
+    {
+        current_sprite = game->p1.front_sprites[sprite_index];
+    } 
+    else if (game->p1.mv_dir == DIR_LEFT)
+    {
+        current_sprite = game->p1.left_sprites[sprite_index];
+    } 
+    else if (game->p1.mv_dir == DIR_RIGHT)
+    {
+        current_sprite = game->p1.right_sprites[sprite_index];
+    } 
+    else
+    {
+        current_sprite = ft_strdup(PLAYER_FRONT_STAND);
+    }
+
+    game->p1.p1 = create_sprite(game, current_sprite);
+    create_character(game->p1.p1, game, x, y);
+}*/
