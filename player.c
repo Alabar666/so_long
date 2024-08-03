3348f11722a57dc0ebb763c09312a08dbd077353
Hugo Marta <hugollmarta@hotmail.com>
2024-08-03 11:57:44 +0100

    working player and clear coment trash

--- player.c
+++ player.c
@@ -18,7 +18,7 @@ void init_player(t_game *game)
     game->p1.p1_p.y = game->map.start_p1_p.y; 
     game->p1.alive = 1;
     game->p1.mv_dir = STAND;
-    game->p1.face = STAND;
+    game->p1.is_moving = 0;
     game->p1.moves = 0;
     game->p1.current_sprite = 0;
 
@@ -37,7 +37,8 @@ void init_player(t_game *game)
     game->p1.right_sprites[0] = ft_strdup(PLAYER_RIGHT_MV1);
     game->p1.right_sprites[1] = ft_strdup(PLAYER_RIGHT_MV2);
     game->p1.right_sprites[2] = ft_strdup(PLAYER_RIGHT_MV3);  
-    
+    game->p1.pl_ballom = ft_strdup(PLAYER_BALLOM);
+
     game->p1.dx = 0;
     game->p1.dy = 0;
     game->p1.steps_remaining = 0;
@@ -45,104 +46,36 @@ void init_player(t_game *game)
     game->p1.update_interval = 1000 / 60;
     game->p1.last_update_time = clock();
     game->p1.accumulated_time = 0;
-
 }
-/*
-void    create_player(t_sprite *sprite, t_game *game, int posx, int posy)
-{
-    int x;
-    int y;
-    unsigned int color;
-	unsigned int trans_color = 0xFFC0CB;
 
-    y = -1;
-	while (++y < sprite->height)
-	{
-		x = -1;
-		while (++x < sprite->width)
-		{
-			color = get_color_in_pixel(sprite, x, y);
-			if (color != trans_color)
-            {
-				put_pixel(game->world,
-					posx + x,
-					posy + y, color);
-			}
-		}
-	}  
-}*/
 
 void	put_player(t_game *game, int sprite_index)
 {
     char *current_sprite;
-    int x = game->p1.p1_p.x;
-    int y = game->p1.p1_p.y;
+    int x;
+    int y;
 
 	x = game->p1.p1_p.x;
 	y = game->p1.p1_p.y;
-
-    if (game->p1.mv_dir == DIR_UP)
-    {
-        if(sprite_index < 3)
-            current_sprite = game->p1.back_sprites[sprite_index];
-        else
-            current_sprite = game->p1.back_sprites[0];
-    }    
-    else if (game->p1.mv_dir == DIR_DOWN)
-    {
-        if(sprite_index < 3)
-            current_sprite = game->p1.front_sprites[sprite_index];
-        else
-            current_sprite = game->p1.front_sprites[0];
-    } 
-    else if (game->p1.mv_dir == DIR_LEFT)
-    {
-        if(sprite_index < 3)
-            current_sprite = game->p1.left_sprites[sprite_index];
-        else
-            current_sprite = game->p1.left_sprites[0];
-    } 
-    else if (game->p1.mv_dir == DIR_RIGHT)
-    {
-        if(sprite_index < 3)
-            current_sprite = game->p1.right_sprites[sprite_index];
-        else
-            current_sprite = game->p1.right_sprites[0];
-    } 
-    else
-        current_sprite = ft_strdup(PLAYER_FRONT_STAND);
-
-    game->p1.p1 = create_sprite(game, current_sprite);
-    create_character(game->p1.p1, game, x, y);
-}
-/*
-void put_player(t_game *game, int sprite_index)
-{
-    char *current_sprite;
-    int x = game->p1.p1_p.x;
-    int y = game->p1.p1_p.y;
-
-    if (game->p1.mv_dir == DIR_UP)
-    {
-        current_sprite = game->p1.back_sprites[sprite_index];
-    }    
-    else if (game->p1.mv_dir == DIR_DOWN)
-    {
-        current_sprite = game->p1.front_sprites[sprite_index];
-    } 
-    else if (game->p1.mv_dir == DIR_LEFT)
-    {
-        current_sprite = game->p1.left_sprites[sprite_index];
-    } 
-    else if (game->p1.mv_dir == DIR_RIGHT)
-    {
-        current_sprite = game->p1.right_sprites[sprite_index];
-    } 
-    else
-    {
-        current_sprite = ft_strdup(PLAYER_FRONT_STAND);
+    if (sprite_index < 0 || sprite_index >= 3) {
+        sprite_index = 0;
     }
+    if (game->p1.mv_dir == DIR_UP)
+        current_sprite = game->p1.back_sprites[sprite_index];
+    else if (game->p1.mv_dir == DIR_DOWN)
+        current_sprite = game->p1.front_sprites[sprite_index];
+    else if (game->p1.mv_dir == DIR_LEFT)
+        current_sprite = game->p1.left_sprites[sprite_index];
+    else if (game->p1.mv_dir == DIR_RIGHT)
+        current_sprite = game->p1.right_sprites[sprite_index];
+    else
+        current_sprite = ft_strdup(PLAYER_FRONT_STAND);
 
     game->p1.p1 = create_sprite(game, current_sprite);
     create_character(game->p1.p1, game, x, y);
-}*/
+    if(game->p1.moves != 0)
+    {
+    game->p1.bl = create_sprite(game, game->p1.pl_ballom);
+    create_character(game->p1.bl, game, x + 40, y - 40);
+    }
+}
