82781b828cbc1f8a6a4d83ddd09ac41cd3e4b5ed
Hugo Marta <hugollmarta@hotmail.com>
2024-08-11 22:09:02 +0100

    norminette

--- moves.c
+++ moves.c
@@ -12,14 +12,24 @@
 
 #include "so_long.h"
 
-
-int key_pressed(int key, t_game *game){
-    
-    if (key == ESC && !game->is_paused)
+/*
+int key_pressed(int key, t_game *game) 
+{
+    if (key == ESC && !game->is_paused) 
     {
         gameover(game);
+        return 0;
     }
-    else if (game->p1.is_moving)
+
+    if (game->battle.is_running) 
+        return handle_battle_keys(key, game);
+    else
+        return handle_player_movement(key, game);
+}
+
+int handle_player_movement(int key, t_game *game) 
+{
+    if (game->p1.is_moving || !game->p1.alive)
         return 0;
     else if (key == KEY_W || key == UP)
     {
@@ -43,6 +53,90 @@ int key_pressed(int key, t_game *game){
     }
     return (0);
 }
+
+int handle_battle_keys(int key, t_game *game) 
+{
+    if (key == ENTER) 
+    {
+        if (game->run_selected) 
+        {
+            game->message_display_duration = 0;
+            while (game->message_display_duration++ < 1250000) 
+            {
+                mlx_string_put(game->mlx, game->win, 
+                    (game->world->width - 200) / 2, 
+                    (game->world->height - 50) / 2, 
+                    0xFFFF00, 
+                    "The Goblin will not allow you to escape.");
+            }
+            return 0;
+        } 
+        else 
+        {   
+            game->wait_input = 0;
+            close_battle(game);  
+            return 0; 
+        }    
+    }
+    battle_keys(key, game);
+    return 0;
+}*/
+
+int key_pressed(int key, t_game *game){
+    
+    if (key == ESC && !game->is_paused)
+    {
+        gameover(game);
+    }
+    if (game->battle.is_running)
+    {
+        if (key == ENTER)
+         {
+            if (game->run_selected)
+            {
+                game->message_display_duration = 0;
+                while(game->message_display_duration++ < 1000000)
+                    mlx_string_put(game->mlx, game->win, ((game->world->width - 200) / 2), ((game->world->height - 50) / 2), 0xFFFF00, "The Goblin will not allow you to escape.");
+                return 0;
+            }
+            else
+            {   
+                printf("test"); 
+             game->wait_input = 0;
+             close_battle(game);  
+             return 0; 
+            }    
+          }
+        battle_keys(key, game);
+    }
+    else
+    {
+    if (game->p1.is_moving || !game->p1.alive)
+        return 0;
+    else if (key == KEY_W || key == UP)
+    {
+        if(game->map.map[(game->p1.p1_p.y / SZ) - 1][game->p1.p1_p.x / SZ].type != '1') 
+            return(game->p1.mv_dir = DIR_UP, move_player(game, 0, -1), 0);
+    }
+    else if (key == KEY_S || key == DOWN)
+    {
+        if(game->map.map[(game->p1.p1_p.y / SZ) + 1][game->p1.p1_p.x / SZ].type != '1') 
+            return(game->p1.mv_dir = DIR_DOWN, move_player(game, 0, +1), 0);
+    }
+    else if (key == KEY_A || key == LEFT)
+    {
+        if(game->map.map[game->p1.p1_p.y / SZ][(game->p1.p1_p.x / SZ) -1].type != '1') 
+            return(game->p1.mv_dir = DIR_LEFT, move_player(game, -1, 0), 0);
+    }
+    else if (key == KEY_D || key == RIGHT)
+    {
+        if(game->map.map[game->p1.p1_p.y / SZ][(game->p1.p1_p.x / SZ) +1].type != '1') 
+            return(game->p1.mv_dir = DIR_RIGHT, move_player(game, 1, 0), 0);
+    }
+    }
+    return (0);
+}
+
  
 void move_player(t_game *game, int dx, int dy)
 {
@@ -142,33 +236,99 @@ void update_map_tiles(t_game *game, int old_x, int old_y, int new_x, int new_y, 
 void check_position(t_game *game, int dx, int dy) 
 {
     t_goblin *cur_gbl = game->gbl;
+    char enemy_type;
+
+    enemy_type = game->map.map[dy / 40][dx / 40].type;
 
     if (dx < 0 || dx >= game->map.width || dy < 0 || dy >= game->map.height) 
         return;
  
-    if (game->map.map[dy / 40][dx/ 40].type == 'C' || game->map.map[dy/40][dx/40].type == 'M') 
+    if (enemy_type == 'C' || enemy_type == 'M') 
     {
         while (cur_gbl != NULL) {
             if (cur_gbl->gbl_p.x == dx && cur_gbl->gbl_p.y == dy && cur_gbl->is_alive) {
                 cur_gbl->is_alive = 0;
                 game->map.map[dy/ 40][dx/ 40].type = 'B';
                 game->map.goblin--;
-                start_battle(game);
+                start_battle(game, enemy_type);
                 break;
             }
             cur_gbl = cur_gbl->next;
         }
     }
-    if(game->map.map[dy / 40][dx/ 40].type == 'M')
+    if(enemy_type == 'M')
     {
+        start_battle(game, enemy_type);
         ft_printf("You are dead!\n");
-        usleep(900000);
-        gameover(game);
+        game->p1.p1 = create_sprite(game, PDEAD);
+        game->p1.alive = 0;
+        if(!game->wait_input && !game->p1.alive)
+            {
+                usleep(900000);
+             gameover(game);
+            }
     }
-    if(game->map.goblin == 0 && game->map.map[dy / 40][dx/ 40].type == 'E')
+    if(game->map.goblin == 0 && enemy_type == 'E')
     {
         ft_printf("You saved the priestess!\n");
         usleep(900000);
         gameover(game);
     }
 }
+
+/*
+void check_position(t_game *game, int dx, int dy) 
+{
+    if (dx < 0 || dx >= game->map.width || dy < 0 || dy >= game->map.height) 
+        return;
+
+    handle_goblins(game, dx, dy);
+    handle_enemies_and_victory(game, dx, dy);
+}
+
+
+
+void handle_goblins(t_game *game, int dx, int dy) 
+{
+    t_goblin *cur_gbl = game->gbl;
+    char enemy_type;
+
+    enemy_type = game->map.map[dy / SZ][dx / SZ].type;
+
+    if (enemy_type != 'C' && enemy_type != 'M')
+        return;
+
+    while (cur_gbl != NULL) {
+        if (cur_gbl->gbl_p.x == dx && cur_gbl->gbl_p.y == dy && cur_gbl->is_alive) {
+            cur_gbl->is_alive = 0;
+            game->map.map[dy / SZ][dx / SZ].type = 'B';
+            game->map.goblin--;
+            start_battle(game, enemy_type);
+            break;
+        }
+        cur_gbl = cur_gbl->next;
+    }
+}
+
+void handle_enemies_and_victory(t_game *game, int dx, int dy) 
+{
+    char enemy_type = game->map.map[dy / SZ][dx / SZ].type;
+
+    if (enemy_type == 'M') {
+        start_battle(game, enemy_type);
+        ft_printf("You are dead!\n");
+        game->p1.p1 = create_sprite(game, PDEAD);
+        game->p1.alive = 0;
+        if (!game->wait_input && !game->p1.alive) {
+            usleep(900000);
+            gameover(game);
+        }
+    }
+
+    if (game->map.goblin == 0 && enemy_type == 'E') {
+        ft_printf("You saved the priestess!\n");
+        usleep(900000);
+        gameover(game);
+    }
+}
+*/
