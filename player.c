c978c6cb804abb7f77ce8772313a03829d13483f
Hugo Marta <hugollmarta@hotmail.com>
2024-08-11 22:09:09 +0100

    norminette

--- player.c
+++ player.c
@@ -21,8 +21,20 @@ void init_player(t_game *game)
     game->p1.is_moving = 0;
     game->p1.moves = 0;
     game->p1.current_sprite = 0;
+    game->p1.dx = 0;
+    game->p1.dy = 0;
+    game->p1.steps_remaining = 0;
+    game->p1.step_size = 0;
+    game->p1.update_interval = 1000 / 60;
+    game->p1.last_update_time = clock();
+    game->p1.accumulated_time = 0;
 
-	game->p1.front_sprites[0] = ft_strdup(PLAYER_FRONT_STAND);
+    init_player_sprites(game);
+}
+
+void init_player_sprites(t_game *game)
+{
+    game->p1.front_sprites[0] = ft_strdup(PLAYER_FRONT_STAND);
     game->p1.front_sprites[1] = ft_strdup(PLAYER_FRONT_MV1);
     game->p1.front_sprites[2] = ft_strdup(PLAYER_FRONT_MV2);
 
@@ -37,15 +49,8 @@ void init_player(t_game *game)
     game->p1.right_sprites[0] = ft_strdup(PLAYER_RIGHT_MV1);
     game->p1.right_sprites[1] = ft_strdup(PLAYER_RIGHT_MV2);
     game->p1.right_sprites[2] = ft_strdup(PLAYER_RIGHT_MV3);  
+    
     game->p1.pl_ballom = ft_strdup(PLAYER_BALLOM);
-
-    game->p1.dx = 0;
-    game->p1.dy = 0;
-    game->p1.steps_remaining = 0;
-    game->p1.step_size = 0;
-    game->p1.update_interval = 1000 / 60;
-    game->p1.last_update_time = clock();
-    game->p1.accumulated_time = 0;
 }
 
 
@@ -57,19 +62,23 @@ void	put_player(t_game *game, int sprite_index)
 
 	x = game->p1.p1_p.x;
 	y = game->p1.p1_p.y;
-    if (sprite_index < 0 || sprite_index >= 3) {
-        sprite_index = 0;
+    if(game->p1.alive)
+    {
+        if (sprite_index < 0 || sprite_index >= 3)
+            sprite_index = 0;        
+        if (game->p1.mv_dir == DIR_UP)
+            current_sprite = game->p1.back_sprites[sprite_index];
+        else if (game->p1.mv_dir == DIR_DOWN)
+            current_sprite = game->p1.front_sprites[sprite_index];
+        else if (game->p1.mv_dir == DIR_LEFT)
+            current_sprite = game->p1.left_sprites[sprite_index];
+        else if (game->p1.mv_dir == DIR_RIGHT)
+            current_sprite = game->p1.right_sprites[sprite_index];
+        else
+            current_sprite = ft_strdup(PLAYER_FRONT_STAND);
     }
-    if (game->p1.mv_dir == DIR_UP)
-        current_sprite = game->p1.back_sprites[sprite_index];
-    else if (game->p1.mv_dir == DIR_DOWN)
-        current_sprite = game->p1.front_sprites[sprite_index];
-    else if (game->p1.mv_dir == DIR_LEFT)
-        current_sprite = game->p1.left_sprites[sprite_index];
-    else if (game->p1.mv_dir == DIR_RIGHT)
-        current_sprite = game->p1.right_sprites[sprite_index];
     else
-        current_sprite = ft_strdup(PLAYER_FRONT_STAND);
+        current_sprite = ft_strdup(PDEAD);
 
     game->p1.p1 = create_sprite(game, current_sprite);
     create_character(game->p1.p1, game, x, y);
